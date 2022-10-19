3d9551a2cc04b1d862a0ee98ea3b69b8d1d23a30
GabrielfNascimento <gf31956@gmail.com>
2022-10-19 13:12:39 -0300

    mongo db dependencie

--- src/infrastructure/http/config/App.ts
+++ src/infrastructure/http/config/App.ts
@@ -1,7 +1,11 @@
 import { Router, Express } from "express";
-
+import setupMiddleware from "./middleware";
+import setupRouter from "./Router";
 export default (app: Express): void => {
   const router = Router();
+
+  setupMiddleware(app);
+  setupRouter(app);
 
   app.use("/teste", router);
 };
--- src/infrastructure/http/config/Middleware.ts
+++ src/infrastructure/http/config/Middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
-import bodyParser from "body-parser";
+import { bodyParser, contentType, cors } from "../middleware";
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
+  app.use(contentType);
 };
