3e3b9a8579d05c90c2edbde8412cdf72543317f6
GabrielfNascimento <gf31956@gmail.com>
2022-10-20 14:19:49 -0300

    route configured

--- src/infrastructure/http/config/Router.ts
+++ src/infrastructure/http/config/Router.ts
@@ -1,6 +1,9 @@
 import { Router, Express } from "express";
+import UserRouter from "../routes/UserRouter";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/fb", router);
+
+  UserRouter(router);
 };
--- src/infrastructure/http/routes/UserRouter.ts
+++ src/infrastructure/http/routes/UserRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ExpressRouterAdapter } from "../../http/adapters/ExpressRouterAdapter";
+import { CreateUserFactory } from "../../factories/CreateUserFactory";
 
 export default (router: Router): void => {
-  router.post("/user", ExpressRouterAdapter());
+  router.post("/user", ExpressRouterAdapter(CreateUserFactory.build()));
 };
