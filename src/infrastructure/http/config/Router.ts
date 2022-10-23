6cb5aa33cac6ddfe228927f507f7f5736439a9d3
GabrielfNascimento <gf31956@gmail.com>
2022-10-23 14:17:01 -0300

    source map on ts config and new url

--- src/infrastructure/http/config/Router.ts
+++ src/infrastructure/http/config/Router.ts
@@ -3,7 +3,7 @@ import UserRouter from "../routes/UserRouter";
 
 export default (app: Express): void => {
   const router = Router();
-  app.use("/fb", router);
+  app.use("/csl", router);
 
   UserRouter(router);
 };
