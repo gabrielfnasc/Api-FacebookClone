2bc3f704eabb16f5c71f7428a0f9587ffe0328d8
GabrielfNascimento <gf31956@gmail.com>
2022-10-25 15:15:47 -0300

    created findById on User Repository

--- src/application/repositories/UserRepository.ts
+++ src/application/repositories/UserRepository.ts
@@ -4,4 +4,5 @@ import { InputCreateUserDto } from "../usecase/user/CreateUserUseCase";
 export interface UserRepository {
   create(data: InputCreateUserDto): Promise<string>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
