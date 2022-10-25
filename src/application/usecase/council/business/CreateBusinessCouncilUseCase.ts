115b5fcb51f57b2b8b43404c7233cb9449aa4b86
GabrielfNascimento <gf31956@gmail.com>
2022-10-25 15:48:06 -0300

    created CreateBusinessCouncilUsecase

--- src/application/usecase/council/business/CreateBusinessCouncilUseCase.ts
+++ src/application/usecase/council/business/CreateBusinessCouncilUseCase.ts
@@ -1,5 +1,8 @@
 import { Council } from "../../../../domain/entities/Council";
 import { Validator } from "../../../../domain/validator/validator";
+import { NotFoundHttpError } from "../../../../infrastructure/http/errors";
+import { BusinessCouncilRepository } from "../../../repositories/council/BusinessCouncilRepository";
+import { UserRepository } from "../../../repositories/UserRepository";
 import { Usecase } from "../../UseCase";
 
 export type InputCreateBusinessCouncilDto = {
@@ -11,9 +14,19 @@ export type InputCreateBusinessCouncilDto = {
 export class CreateBusinessCouncilUseCase
   implements Usecase<InputCreateBusinessCouncilDto, void>
 {
-  constructor(private readonly validator: Validator) {}
+  constructor(
+    private readonly validator: Validator,
+    private readonly userRepo: UserRepository,
+    private readonly businessCouncilRepo: BusinessCouncilRepository
+  ) {}
   async execute(data: InputCreateBusinessCouncilDto): Promise<void> {
     this.validator.validate(data);
-    const { userId, council } = data;
+
+    const user = await this.userRepo.findById(data.userId);
+
+    if (!user) {
+      throw new NotFoundHttpError("Nenhum usuário encontrado!");
+    }
+    await this.businessCouncilRepo.create(data);
   }
 }
