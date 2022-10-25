c3fa367143059b66c9b184f6656c934a5ea491af
GabrielfNascimento <gf31956@gmail.com>
2022-10-25 15:18:47 -0300

    created method findById on UserRepositoryMongoDb

--- src/infrastructure/database/mongodb/UserRepositoryMongoDb.ts
+++ src/infrastructure/database/mongodb/UserRepositoryMongoDb.ts
@@ -3,6 +3,7 @@ import { UserRepository } from "../../../application/repositories/UserRepository
 import { InputCreateUserDto } from "../../../application/usecase/user/CreateUserUseCase";
 import { MongoHelper } from "./MongoHelper";
 import { User } from "../../../domain/entities/User";
+import { ObjectId } from "mongodb";
 
 export class UserRepositoryMongoDB
   extends BaseMongoRepository
@@ -20,5 +21,14 @@ export class UserRepositoryMongoDB
     const projection = { isActive: false, password: false };
     const user = await this.getCollection.findOne(query, { projection });
     return user && MongoHelper.map(user);
+  }
+
+  async findById(id: string): Promise<User> {
+    const query = { _id: new ObjectId(id), isActive: true };
+    const projection = { isActive: false, createdAt: false };
+    const userDocument = await this.getCollection.findOne(query, {
+      projection,
+    });
+    return userDocument && MongoHelper.map(userDocument);
   }
 }
