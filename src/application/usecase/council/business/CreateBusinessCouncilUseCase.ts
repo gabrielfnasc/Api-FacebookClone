a42208789472bccc3d5cc19ccd34a08ed25434c1
GabrielfNascimento <gf31956@gmail.com>
2022-10-25 15:10:19 -0300

    created the half of the usecase

--- src/application/usecase/council/business/CreateBusinessCouncilUseCase.ts
+++ src/application/usecase/council/business/CreateBusinessCouncilUseCase.ts
@@ -1,7 +1,19 @@
 import { Council } from "../../../../domain/entities/Council";
+import { Validator } from "../../../../domain/validator/validator";
+import { Usecase } from "../../UseCase";
 
 export type InputCreateBusinessCouncilDto = {
   userId: string;
   council: Council;
   createdAt: Date;
 };
+
+export class CreateBusinessCouncilUseCase
+  implements Usecase<InputCreateBusinessCouncilDto, void>
+{
+  constructor(private readonly validator: Validator) {}
+  async execute(data: InputCreateBusinessCouncilDto): Promise<void> {
+    this.validator.validate(data);
+    const { userId, council } = data;
+  }
+}
