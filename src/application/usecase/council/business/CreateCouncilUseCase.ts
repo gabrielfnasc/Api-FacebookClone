b0b4e756f37b4983f2d1a020ef6fb56a6756c7ca
GabrielfNascimento <gf31956@gmail.com>
2022-10-29 17:19:56 -0300

    added type on council and created rule on usecase

--- src/application/usecase/council/business/CreateCouncilUseCase.ts
+++ src/application/usecase/council/business/CreateCouncilUseCase.ts
@@ -5,6 +5,7 @@ import { NotFoundHttpError } from "../../../../infrastructure/http/errors";
 import { CouncilRepository } from "../../../repositories/CouncilRepository";
 import { UserRepository } from "../../../repositories/UserRepository";
 import { Usecase } from "../../UseCase";
+import { TypeRepository } from "../../../repositories/TypeRepository";
 
 export type InputCreateCouncilDto = {
   userId: string;
@@ -18,21 +19,26 @@ export class CreateCouncilUseCase
   constructor(
     private readonly validator: Validator,
     private readonly userRepo: UserRepository,
-    private readonly businessCouncilRepo: CouncilRepository
+    private readonly businessCouncilRepo: CouncilRepository,
+
+    private readonly type: TypeRepository
   ) {}
   async execute(data: InputCreateCouncilDto): Promise<void> {
     this.validator.validate(data);
-
-    // check the length of the council content
-    const minLenght: number = 10;
-    if (data.council.content.length < minLenght) {
-      throw new RequiredMinLengthDomainError("content", minLenght);
-    }
 
     //check if user exists
     const user = await this.userRepo.findById(data.userId);
     if (!user) {
       throw new NotFoundHttpError("Nenhum usuário encontrado!");
+    }
+    // check the length of the council content
+    const minLenght: number = 10;
+    if (data.council.content.length < minLenght) {
+      throw new RequiredMinLengthDomainError("content", minLenght);
+    }
+    const typeName = await this.type.find(data.council.type.name);
+    if (!typeName) {
+      throw new NotFoundHttpError("Sorry! we don't have this council type yet");
     }
 
     //TODO
--- src/domain/entities/Council.ts
+++ src/domain/entities/Council.ts
@@ -1,4 +1,8 @@
+import { Type } from "./Type";
+
 export interface Council {
   content: string;
   createdAt: Date;
+
+  type: Type;
 }
