505417bd64c7477271e0b7a90bae3b3a90240cfb
GabrielfNascimento <gf31956@gmail.com>
2022-11-02 14:39:34 -0300

    created route from FindCouncilByContent

--- src/infrastructure/http/routes/CouncilRouter.ts
+++ src/infrastructure/http/routes/CouncilRouter.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreateCouncilFactory } from "../../factories/council/CreateCouncilFactory";
+import { FindCouncilByContentFactory } from "../../factories/council/FindCouncilByContentFactory";
 import { ExpressRouterAdapter } from "../adapters/ExpressRouterAdapter";
 
 export default (router: Router): void => {
   router.post(
     "/council/:userId",
     ExpressRouterAdapter(CreateCouncilFactory.build())
+  );
+  router.get(
+    "/council",
+    ExpressRouterAdapter(FindCouncilByContentFactory.build())
   );
 };
