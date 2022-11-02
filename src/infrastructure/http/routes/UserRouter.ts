0f44009bacf9aef36f72119bea7fde267d902d69
GabrielfNascimento <gf31956@gmail.com>
2022-11-02 11:39:27 -0300

    create deleteUser route

--- src/infrastructure/http/routes/UserRouter.ts
+++ src/infrastructure/http/routes/UserRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { ExpressRouterAdapter } from "../../http/adapters/ExpressRouterAdapter";
 import { CreateUserFactory } from "../../factories/user/CreateUserFactory";
+import { DeleteUserFactory } from "../../factories/user/DeleteUserFactory";
 
 export default (router: Router): void => {
   router.post("/user", ExpressRouterAdapter(CreateUserFactory.build()));
+  router.delete("/user", ExpressRouterAdapter(DeleteUserFactory.build()));
 };
