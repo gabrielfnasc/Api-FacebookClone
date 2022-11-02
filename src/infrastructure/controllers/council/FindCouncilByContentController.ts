e2c57bcdbd51470d125619e647c0a7b72b2e84c6
GabrielfNascimento <gf31956@gmail.com>
2022-11-02 14:46:00 -0300

    added validator on FindCouncilByContentController

--- src/infrastructure/controllers/council/FindCouncilByContentController.ts
+++ src/infrastructure/controllers/council/FindCouncilByContentController.ts
@@ -1,4 +1,5 @@
 import { FindCouncilByContentUseCase } from "../../../application/usecase/council/FindCouncilByContentUseCase";
+import { Validator } from "../../../domain/validator/validator";
 import { HttpResponse } from "../../http/presentation/controllers/helpers/Http";
 import {
   ok,
@@ -11,9 +12,13 @@ export type FindCouncilByContentRequest = {
 };
 
 export class FindCouncilByContentController implements BaseController {
-  constructor(private readonly usecase: FindCouncilByContentUseCase) {}
+  constructor(
+    private readonly usecase: FindCouncilByContentUseCase,
+    private readonly validator: Validator
+  ) {}
   async handle(request: FindCouncilByContentRequest): Promise<HttpResponse> {
     try {
+      this.validator.validate(request);
       const output = await this.usecase.execute(request);
       return ok(output);
     } catch (error) {
