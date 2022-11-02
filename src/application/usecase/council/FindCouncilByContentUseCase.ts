8e3411a5e788fddd040cd9b60c5afcc80ef13e47
GabrielfNascimento <gf31956@gmail.com>
2022-11-02 14:49:33 -0300

    created validatorRequest on FindCouncilByContentFactory and deleted validator from FindCO

--- src/application/usecase/council/FindCouncilByContentUseCase.ts
+++ src/application/usecase/council/FindCouncilByContentUseCase.ts
@@ -1,5 +1,4 @@
 import { Council } from "../../../domain/entities/Council";
-import { Validator } from "../../../domain/validator/validator";
 import { NotFoundHttpError } from "../../../infrastructure/http/errors";
 import { CouncilRepository } from "../../repositories/CouncilRepository";
 import { Usecase } from "../UseCase";
@@ -16,14 +15,10 @@ export class FindCouncilByContentUseCase
   implements
     Usecase<InputFindCouncilByContentDto, OutputFindCouncilByContentDto>
 {
-  constructor(
-    private readonly repo: CouncilRepository,
-    private readonly validator: Validator
-  ) {}
+  constructor(private readonly repo: CouncilRepository) {}
   async execute(
     data: InputFindCouncilByContentDto
   ): Promise<OutputFindCouncilByContentDto> {
-    this.validator.validate(data);
     const council = await this.repo.find(data.content);
     if (!council) {
       throw new NotFoundHttpError("Council not found!");
--- src/infrastructure/factories/council/FindCouncilByContentFactory.ts
+++ src/infrastructure/factories/council/FindCouncilByContentFactory.ts
@@ -1,4 +1,5 @@
 import { FindCouncilByContentUseCase } from "../../../application/usecase/council/FindCouncilByContentUseCase";
+import { ValidatorRequiredParam } from "../../../application/validator/ValidatorRequiredParam";
 import { BaseController } from "../../controllers/BaseController";
 import { FindCouncilByContentController } from "../../controllers/council/FindCouncilByContentController";
 import { CouncilRepositoryMongoDb } from "../../database/mongodb/CouncilRepositoryMongoDb";
@@ -6,7 +7,9 @@ import { CouncilRepositoryMongoDb } from "../../database/mongodb/CouncilReposito
 export class FindCouncilByContentFactory {
   static build(): BaseController {
     const repo = new CouncilRepositoryMongoDb();
+
+    const validatorRequest = new ValidatorRequiredParam("content");
     const usecase = new FindCouncilByContentUseCase(repo);
-    return new FindCouncilByContentController(usecase);
+    return new FindCouncilByContentController(usecase, validatorRequest);
   }
 }
