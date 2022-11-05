c802559a1ba221d0cbe876241b2798849d4aa032
GabrielfNascimento <gf31956@gmail.com>
2022-11-05 12:24:07 -0300

    created class LoginUseCase and added validator as dependency injection

--- src/application/usecase/user/LoginUseCase.ts
+++ src/application/usecase/user/LoginUseCase.ts
@@ -1,3 +1,6 @@
+import { Validator } from "../../../domain/validator/validator";
+import { Usecase } from "../UseCase";
+
 export type InputLoginDto = {
   email: string;
   password: string;
@@ -6,3 +9,12 @@ export type InputLoginDto = {
 export type OutputLoginDto = {
   accessToken: string;
 };
+
+export class LoginUseCase implements Usecase<InputLoginDto, OutputLoginDto> {
+  constructor(private readonly validator: Validator, private readonly repository : ) {}
+  async execute(data: InputLoginDto): Promise<OutputLoginDto> {
+    this.validator.validate(data);
+
+  const user = await this.repository
+  }
+}
