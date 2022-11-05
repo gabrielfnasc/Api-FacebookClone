7bcc6dc7d1e7ea4c0d4c7d3efc53850b2ed37d6d
GabrielfNascimento <gf31956@gmail.com>
2022-11-05 18:54:48 -0300

    created login route

--- src/infrastructure/http/routes/UserRouter.ts
+++ src/infrastructure/http/routes/UserRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ExpressRouterAdapter } from "../../http/adapters/ExpressRouterAdapter";
 import { CreateUserFactory } from "../../factories/user/CreateUserFactory";
 import { DeleteUserFactory } from "../../factories/user/DeleteUserFactory";
+import { LoginFactory } from "../../factories/user/LoginFactory";
 
 export default (router: Router): void => {
   router.post("/user", ExpressRouterAdapter(CreateUserFactory.build()));
@@ -9,4 +10,5 @@ export default (router: Router): void => {
     "/user/:userId",
     ExpressRouterAdapter(DeleteUserFactory.build())
   );
+  router.get("/login", ExpressRouterAdapter(LoginFactory.build()));
 };
