4f79fc639ea01a7cb190edf6a61422bd29745bcd
GabrielfNascimento <gf31956@gmail.com>
2022-11-05 18:03:24 -0300

    adjusted position on UserRepository and created rule on LoginUseCase

--- src/application/repositories/UserRepository.ts
+++ src/application/repositories/UserRepository.ts
@@ -5,8 +5,6 @@ export interface UserRepository {
   create(data: InputCreateUserDto): Promise<string>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
-
   delete(userId: string): Promise<void>;
-
   login(email: string): Promise<User>;
 }
--- src/application/usecase/user/LoginUseCase.ts
+++ src/application/usecase/user/LoginUseCase.ts
@@ -1,4 +1,6 @@
 import { Validator } from "../../../domain/validator/validator";
+import { NotFoundHttpError } from "../../../infrastructure/http/errors";
+import { UserRepository } from "../../repositories/UserRepository";
 import { Usecase } from "../UseCase";
 
 export type InputLoginDto = {
@@ -11,10 +13,16 @@ export type OutputLoginDto = {
 };
 
 export class LoginUseCase implements Usecase<InputLoginDto, OutputLoginDto> {
-  constructor(private readonly validator: Validator, private readonly repository : ) {}
+  constructor(
+    private readonly validator: Validator,
+    private readonly repository: UserRepository
+  ) {}
   async execute(data: InputLoginDto): Promise<OutputLoginDto> {
     this.validator.validate(data);
 
-  const user = await this.repository
+    const user = await this.repository.findByEmail(data.email);
+    if (!user) {
+      throw new NotFoundHttpError("User not found!");
+    }
   }
 }
