00ff2abb10eda305f8654ff630e484c2be68a6f5
GabrielfNascimento <gf31956@gmail.com>
2022-11-07 23:29:35 -0300

    created deleted method on CouncilRepository

--- src/application/repositories/CouncilRepository.ts
+++ src/application/repositories/CouncilRepository.ts
@@ -1,5 +1,6 @@
 import { Council } from "../../domain/entities/Council";
 import { InputCreateCouncilDto } from "../usecase/council/CreateCouncilUseCase";
+import { InputDeleteCouncilUseCase } from "../usecase/council/DeleteCouncilUseCase";
 
 export interface CouncilRepository {
   create(data: InputCreateCouncilDto): Promise<void>;
@@ -7,4 +8,6 @@ export interface CouncilRepository {
   find(content: string): Promise<Council>;
 
   findOneCouncil(content: string): Promise<Council>;
+
+  delete(data: InputDeleteCouncilUseCase): Promise<void>;
 }
