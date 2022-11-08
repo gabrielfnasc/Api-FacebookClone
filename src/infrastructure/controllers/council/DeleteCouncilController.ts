0fd6a6fe7278d013c5ba440a0e9962c137cf2409
GabrielfNascimento <gf31956@gmail.com>
2022-11-07 23:23:56 -0300

    created DeleteCouncilController

---
+++ src/infrastructure/controllers/council/DeleteCouncilController.ts
@@ -0,0 +1,16 @@
+import { HttpResponse } from "../../http/presentation/controllers/helpers/Http";
+import { serverError } from "../../http/presentation/controllers/helpers/HttpHelper";
+import { BaseController } from "../BaseController";
+
+export type DeleteCouncilRequestDto = {
+  councilId: string;
+};
+
+export class DeleteCouncilController implements BaseController {
+  async handle(request: DeleteCouncilController): Promise<HttpResponse> {
+    try {
+    } catch (error) {
+      return serverError(error as Error);
+    }
+  }
+}
