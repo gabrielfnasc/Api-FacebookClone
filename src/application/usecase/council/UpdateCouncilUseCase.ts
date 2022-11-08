7a4ebaa027d45aeb6f4b15af59261a466d114e38
GabrielfNascimento <gf31956@gmail.com>
2022-11-08 16:35:26 -0300

    UpdateCouncilUseCase created

---
+++ src/application/usecase/council/UpdateCouncilUseCase.ts
@@ -0,0 +1,24 @@
+import { NotFoundHttpError } from "../../../infrastructure/http/errors";
+import { CouncilRepository } from "../../repositories/CouncilRepository";
+import { UserRepository } from "../../repositories/UserRepository";
+import { Usecase } from "../UseCase";
+
+export type InputUpdateCouncilDto = {
+  userId: string;
+  councilId: string;
+};
+
+export class UpdateCouncilUseCase
+  implements Usecase<InputUpdateCouncilDto, void>
+{
+  constructor(
+    private readonly userRepo: UserRepository,
+    private readonly councilRepository: CouncilRepository
+  ) {}
+  async execute(data: InputUpdateCouncilDto): Promise<void> {
+    const user = await this.userRepo.findById(data.userId);
+    if (!user) {
+      throw new NotFoundHttpError("User not found!");
+    }
+  }
+}
