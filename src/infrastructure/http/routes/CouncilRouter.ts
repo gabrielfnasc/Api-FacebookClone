8486dbec4dec195039af387bf080dc45bab4837f
GabrielfNascimento <gf31956@gmail.com>
2022-11-08 00:11:09 -0300

    created DeleteCouncilRouter

--- src/infrastructure/http/routes/CouncilRouter.ts
+++ src/infrastructure/http/routes/CouncilRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateCouncilFactory } from "../../factories/council/CreateCouncilFactory";
+import { DeleteCouncilFactory } from "../../factories/council/DeleteCouncilFactory";
 import { FindCouncilByContentFactory } from "../../factories/council/FindCouncilByContentFactory";
 import { ExpressRouterAdapter } from "../adapters/ExpressRouterAdapter";
 
@@ -11,5 +12,10 @@ export default (router: Router): void => {
   router.get(
     "/council",
     ExpressRouterAdapter(FindCouncilByContentFactory.build())
+  );
+
+  router.delete(
+    "council/:userId",
+    ExpressRouterAdapter(DeleteCouncilFactory.build())
   );
 };
