5181b0b5ec13fabbb6ab077e68e5c02e7f5d1079
GabrielfNascimento <gf31956@gmail.com>
2022-11-08 16:52:36 -0300

    UpdateCouncilController implemented

--- src/infrastructure/controllers/council/UpdateCouncilController.ts
+++ src/infrastructure/controllers/council/UpdateCouncilController.ts
@@ -1,20 +1,29 @@
+import { UpdateCouncilUseCase } from "../../../application/usecase/council/UpdateCouncilUseCase";
 import { Validator } from "../../../domain/validator/validator";
 import { HttpResponse } from "../../http/presentation/controllers/helpers/Http";
-import { serverError } from "../../http/presentation/controllers/helpers/HttpHelper";
+import {
+  ok,
+  serverError,
+} from "../../http/presentation/controllers/helpers/HttpHelper";
 import { BaseController } from "../BaseController";
 
 export type UpdateCouncilRequestDto = {
   userId: string;
   councilId: string;
-content : string;
+  content: string;
 };
 
 export class UpdateCouncilController implements BaseController {
-  constructor(private readonly validator: Validator,private readonly usecase :) {}
+  constructor(
+    private readonly validator: Validator,
+    private readonly usecase: UpdateCouncilUseCase
+  ) {}
+
   async handle(request: UpdateCouncilRequestDto): Promise<HttpResponse> {
     try {
-
       this.validator.validate(request);
+      await this.usecase.execute(request);
+      return ok("Council updated successfully!");
     } catch (error) {
       return serverError(error as Error);
     }
