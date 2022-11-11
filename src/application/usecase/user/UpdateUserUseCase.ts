190f490a1d7103fc696590ada049787f164035e6
GabrielfNascimento <gf31956@gmail.com>
2022-11-11 11:46:18 -0300

    created UpdateUserUseCase

---
+++ src/application/usecase/user/UpdateUserUseCase.ts
@@ -0,0 +1,31 @@
+import { User } from "../../../domain/entities/User";
+import { Validator } from "../../../domain/validator/validator";
+import { NotFoundHttpError } from "../../../infrastructure/http/errors";
+import { UserRepository } from "../../repositories/UserRepository";
+import { Usecase } from "../UseCase";
+
+export type InputUpdateUserDto = {
+  userId: string;
+  email: string;
+  name: string;
+};
+
+export type OutputUpdateUserDto = {
+  user: User;
+};
+
+export class UpdateUserUseCase
+  implements Usecase<InputUpdateUserDto, OutputUpdateUserDto>
+{
+  constructor(
+    private readonly validator: Validator,
+    private readonly repository: UserRepository
+  ) {}
+  async execute(data: InputUpdateUserDto): Promise<OutputUpdateUserDto> {
+    this.validator.validate(data);
+    let user = await this.repository.findById(data.userId);
+    if (!user) {
+      throw new NotFoundHttpError("User not found!");
+    }
+  }
+}
