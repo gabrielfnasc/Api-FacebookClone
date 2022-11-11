15c5aeb37a9116525ddef21b9e5be752add6b0c8
GabrielfNascimento <gf31956@gmail.com>
2022-11-11 11:16:42 -0300

    created route from FindUserById

--- src/infrastructure/http/routes/UserRouter.ts
+++ src/infrastructure/http/routes/UserRouter.ts
@@ -3,6 +3,7 @@ import { ExpressRouterAdapter } from "../../http/adapters/ExpressRouterAdapter";
 import { CreateUserFactory } from "../../factories/user/CreateUserFactory";
 import { DeleteUserFactory } from "../../factories/user/DeleteUserFactory";
 import { LoginFactory } from "../../factories/user/LoginFactory";
+import { FindUSerByIdFactory } from "../../factories/user/FindUserByIdFactory";
 
 export default (router: Router): void => {
   router.post("/user", ExpressRouterAdapter(CreateUserFactory.build()));
@@ -11,4 +12,6 @@ export default (router: Router): void => {
     ExpressRouterAdapter(DeleteUserFactory.build())
   );
   router.post("/login", ExpressRouterAdapter(LoginFactory.build()));
+
+  router.get("user/:userId", ExpressRouterAdapter(FindUSerByIdFactory.build()));
 };
