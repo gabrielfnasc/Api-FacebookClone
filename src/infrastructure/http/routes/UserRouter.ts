e3064854d57ee5a46385691ffe782a1d2a55ffe3
GabrielfNascimento <gf31956@gmail.com>
2022-11-11 14:26:37 -0300

    created route from updateUser

--- src/infrastructure/http/routes/UserRouter.ts
+++ src/infrastructure/http/routes/UserRouter.ts
@@ -4,6 +4,7 @@ import { CreateUserFactory } from "../../factories/user/CreateUserFactory";
 import { DeleteUserFactory } from "../../factories/user/DeleteUserFactory";
 import { LoginFactory } from "../../factories/user/LoginFactory";
 import { FindUSerByIdFactory } from "../../factories/user/FindUserByIdFactory";
+import { UpdateUserFactory } from "../../factories/user/UpdateUserFactory";
 
 export default (router: Router): void => {
   router.post("/user", ExpressRouterAdapter(CreateUserFactory.build()));
@@ -17,4 +18,6 @@ export default (router: Router): void => {
     "/user/:userId",
     ExpressRouterAdapter(FindUSerByIdFactory.build())
   );
+
+  router.put("/user/:userId", ExpressRouterAdapter(UpdateUserFactory.build()));
 };
