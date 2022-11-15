317e47dd09ab1fdbe8c3ca1d3adacaaf534f5202
GabrielfNascimento <gf31956@gmail.com>
2022-11-15 16:01:17 -0300

    jest configured

---
+++ test/jest.config.js
@@ -0,0 +1,12 @@
+import { resolve } from 'path';
+const root = resolve(__dirname, '..');
+const rootConfig = require(`${root}/jest.config.js`);
+
+export default {
+    ...rootConfig, ...{
+        rootDir: root,
+        displayName: "end2end-tests",
+        setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+        testMatch: ["<rootDir>/test/**/*.test.ts"],
+    }
+}
