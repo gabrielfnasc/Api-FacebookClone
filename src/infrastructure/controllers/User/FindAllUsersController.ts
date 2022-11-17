d2c05817a2203050bdcb980abcad8fa91e30bcf3
GabrielfNascimento <gf31956@gmail.com>
2022-11-17 08:36:40 -0300

    created FindAllUsersController

---
+++ src/infrastructure/controllers/User/FindAllUsersController.ts
@@ -0,0 +1,9 @@
+import { HttpResponse } from "@src/infrastructure/http/presentation/controllers/helpers";
+import { BaseController } from "../BaseController";
+
+export class FindAllUsersController implements BaseController {
+    constructor(private readonly usecase :){}
+ async  handle(): Promise<HttpResponse> {
+   
+  }
+}
