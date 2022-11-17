f26bbf50e19d5117f8df38689f60450d7571814b
GabrielfNascimento <gf31956@gmail.com>
2022-11-17 08:54:47 -0300

    updated index

--- src/infrastructure/controllers/User/index.ts
+++ src/infrastructure/controllers/User/index.ts
@@ -3,3 +3,4 @@ export * from "./DeleteUserController";
 export * from "./FindUserByIdController";
 export * from "./UpdateUserController";
 export * from "./LoginController";
+export * from "./FindAllUsersController";
