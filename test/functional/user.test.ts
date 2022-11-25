67f95e2ea0928a2c7f9e3a9eaf76181b55687550
GabrielfNascimento <gf31956@gmail.com>
2022-11-25 17:06:41 -0300

    added missing configs

---
+++ test/functional/user.test.ts
@@ -0,0 +1,24 @@
+import { BCryptAdapter } from '@src/infrastructure/adapters';
+
+describe('Users functional tests', () => {
+  const bcrypt = new BCryptAdapter(12);
+
+  describe('When creating a new user', () => {
+    it('should successfully create a new user with encrypted password', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+      await expect(bcrypt.comparer(newUser.password, response.body.password)).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
+    });
+  });
+});
