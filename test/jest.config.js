f06c8c7853cbc06112d14bfcc8f56ed1ccd3f85b
GabrielfNascimento <gf31956@gmail.com>
2022-11-27 11:27:05 -0300

    jest already working

--- test/jest.config.js
+++ test/jest.config.js
@@ -3,10 +3,10 @@ const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
 export default {
-    ...rootConfig, ...{
-        rootDir: root,
-        displayName: "end2end-tests",
-        setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
-        testMatch: ["<rootDir>/test/**/*.test.ts"],
-    }
+  ...rootConfig, ...{
+    rootDir: root,
+    displayName: "end2end-tests",
+    setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+    testMatch: ["<rootDir>/test/**/*.test.ts"]
+  }
 }
