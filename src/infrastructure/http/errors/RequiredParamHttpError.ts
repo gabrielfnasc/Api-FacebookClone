cf694c5f1d71f8dc39a414f9d7779cfd33559a7b
GabrielfNascimento <gf31956@gmail.com>
2022-12-02 15:28:36 -0300

    feat : user tests

--- src/infrastructure/http/errors/RequiredParamHttpError.ts
+++ src/infrastructure/http/errors/RequiredParamHttpError.ts
@@ -1,9 +1,9 @@
-import { ApiError } from "@src/domain/erros";
-import { HttpStatusCode } from "@src/infrastructure/http/presentation/controllers/helpers";
+import { ApiError } from '@src/domain/erros';
+import { HttpStatusCode } from '@src/infrastructure/http/presentation/controllers/helpers';
 
 export class RequiredParamHttpError extends ApiError {
   constructor(paramName: string) {
-    super(`${paramName} é obrigatório!`, HttpStatusCode.BAD_REQUEST);
-    this.name = "RequiredParamHttpError";
+    super(`${paramName} is required!`, HttpStatusCode.BAD_REQUEST);
+    this.name = 'RequiredParamHttpError';
   }
 }
--- test/functional/user.test.ts
+++ test/functional/user.test.ts
@@ -1,24 +1,35 @@
-import { BCryptAdapter } from '@src/infrastructure/adapters/BCryptAdapter';
+test('Create User Successfully', () => {
+  const user = {
+    name: 'John Doe',
+    email: 'john@mail.com',
+    password: 12345,
+  };
 
-describe('Users functional tests', () => {
-  const bcrypt = new BCryptAdapter(12);
-
-  describe('When creating a new user', () => {
-    it('should successfully create a new user with encrypted password', async () => {
-      const newUser = {
-        name: 'John Doe',
-        email: 'john@mail.com',
-        password: '1234',
-      };
-      const response = await global.testRequest.post('/users').send(newUser);
-      expect(response.status).toBe(201);
-      await expect(bcrypt.comparer(newUser.password, response.body.password)).resolves.toBeTruthy();
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          ...newUser,
-          ...{ password: expect.any(String) },
-        })
-      );
-    });
+  expect.objectContaining({
+    ...user,
+    ...{ password: expect.any(Number) },
   });
+});
+test('Find All Users Succesfully', () => {
+  const users = [
+    { name: 'Tupac Shakur', email: 'tupac@mail.com', password: 12345 },
+    { name: 'Michael Jackson', email: 'mike@mail.com', password: 12345 },
+    { name: 'Kurt Cobain', email: 'comeasyouare@mail.com', password: 12345 },
+  ];
+
+  expect.objectContaining({
+    ...users,
+    ...{ password: expect.any(Number) },
+  });
+});
+test('Create User Without Email Field', () => {
+  const login = {
+    email: '',
+    password: 12345,
+  };
+
+  const validateMessage = 'email is required';
+
+  expect.objectContaining({ ...login });
+  expect.stringMatching(validateMessage);
 });
