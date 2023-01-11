82791590f092966cc986832fca2c73a84cbf919d
GabrielfNascimento <gf31956@gmail.com>
2023-01-11 12:17:06 -0300

    feat : mongo url

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  url: process.env.MONGO_URL || "mongodb://localhost:27017/api-counselor",
+  url: process.env.MONGO_URL || 'mongodb://mongodb:27017/api-counselor',
   port: process.env.PORT || 5050,
-  jwtSecretKey: process.env.JWT_SECRET || "tj67O==5H",
+  jwtSecretKey: process.env.JWT_SECRET || 'tj67O==5H',
 };
