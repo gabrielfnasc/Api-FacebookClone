bc9af7ab136647224f619a0eb98e7674120c62e1
GabrielfNascimento <gf31956@gmail.com>
2023-01-11 15:27:17 -0300

    changed db host

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
+  url: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/',
   port: process.env.PORT || 5050,
   jwtSecretKey: process.env.JWT_SECRET || 'tj67O==5H',
 };
