6f85d887659069316598830219ae900c601a861e
GabrielfNascimento <gf31956@gmail.com>
2023-01-16 14:54:21 -0300

    changed db port

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/counselor-api',
+  url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27018/counselor-api',
   port: process.env.PORT || 5050,
   jwtSecretKey: process.env.JWT_SECRET || 'tj67O==5H',
 };
